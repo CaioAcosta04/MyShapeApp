687c5c746a84f478e4c3eb9499b928f0432ffa1d
Caio Acosta <caioacosta4@gmail.com>
2024-04-30 10:13:42 -0300

    FlatList

--- Screens/HomePage.jsx
+++ Screens/HomePage.jsx
@@ -1,10 +1,12 @@
-import { Text, View, StyleSheet, TouchableOpacity, TextInput} from 'react-native'
-import React, { Component, useState } from 'react'
+import { Text, View, StyleSheet, TouchableOpacity, TextInput, FlatList} from 'react-native'
+import React, { useState } from 'react'
 
 import { Feather, AntDesign } from '@expo/vector-icons'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import MainHeader from '../Components/MainHeader'
 import CreateWorPage from './CreateWorPage'
+
+const keyExtractor = ({ id })=> id.toString();
 
 
 const HomePage = (props) => {
@@ -18,18 +20,41 @@ const HomePage = (props) => {
       setWorkout(false)
     }
 
+    const workoutList = [
+      {id: 0, nome: "Treino de Bumbum", quantidade: 50},
+      {id: 1, nome: "Treino de Peito", quantidade: 8},
+      {id: 2, nome: "Treino de Costas", quantidade: 5},
+      {id: 3, nome: "Treino de Tríceps", quantidade: 9},
+      {id: 4, nome: "Treino de Cotovelo", quantidade: 13}
+    ]
+
     return (
 
       <SafeAreaView style={styles.container}>
         <MainHeader/>
         {workout && <CreateWorPage onClose={closeWorkout}/>}
+
         <View style={styles.searchBar}>
           <AntDesign name="search1" size={30} color={'gray'}/>
           <TextInput style={styles.input} placeholder='Buscar treino'/>
         </View>
+
         <View style={styles.workoutsView}>
-          
+          <FlatList 
+            data={workoutList}
+            renderItem={({item})=>{
+              return(
+                <View style={styles.workoutItemView} key={item.id}>
+                  <View style={styles.workoutItemTextView}>
+                    <Text style={styles.workoutItemTitle}>{item.nome}</Text>
+                    <Text style={styles.workoutItemQtd}>Quantidade de exercícios: {item.quantidade}</Text>
+                  </View>
+                </View>
+              )
+            }}
+          />
         </View>
+
         <View style={styles.btnAddView}>
           <TouchableOpacity
             onPress={openWorkout}
@@ -37,6 +62,7 @@ const HomePage = (props) => {
             <Feather name="plus-circle" size={48} color={'gray'}/>
           </TouchableOpacity>
         </View>
+
       </SafeAreaView>
     )
   }
@@ -67,8 +93,6 @@ const styles = StyleSheet.create({
   workoutsView: {
     height: '70%',
     width: '90%',
-    backgroundColor: '#E7E7E7',
-
   },
   btnAddView: {
     height: 50,
@@ -81,7 +105,27 @@ const styles = StyleSheet.create({
     width: '100%',
     marginLeft: 10,
     fontSize: 18
-  }
+  },
+  workoutItemView:{
+    height: 120,
+    backgroundColor: '#E7E7E7',
+    marginBottom:20,
+    padding: 10,
+    display: 'flex',
+    alignItems: 'center'
+  },
+  workoutItemTextView:{
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  workoutItemTitle: {
+    fontSize: 30,
+    marginBottom: 15
+  },
+  workoutItemQtd:{
+    fontSize: 20
+  },
 })
 
 export default HomePage
