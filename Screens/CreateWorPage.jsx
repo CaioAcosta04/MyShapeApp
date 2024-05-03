2ea0dceed49ab6240b9e8dfbea2884322833dc4c
Caio Acosta <caioacosta4@gmail.com>
2024-05-03 10:00:36 -0300

    Tela Create Workout

--- Screens/CreateWorPage.jsx
+++ Screens/CreateWorPage.jsx
@@ -1,5 +1,8 @@
-import { Modal, Text, View, StyleSheet,TouchableOpacity } from 'react-native'
+import { Modal, Text, View, StyleSheet,TouchableOpacity, TextInput } from 'react-native'
 import React, { Component } from 'react'
+
+import { EvilIcons } from '@expo/vector-icons'
+
 
 const CreateWorPage = (props) => {
     return (
@@ -7,12 +10,29 @@ const CreateWorPage = (props) => {
         animationType='slide'
       >
         <View style={styles.container}>
-            <Text>New Workout</Text>
-            <TouchableOpacity
-                onPress={props.onClose}
-            >
-                <Text style={styles.backText}>Voltar</Text>
-            </TouchableOpacity>
+            <View style={styles.nameView}>
+              <View style={styles.infosNameView}>
+                <TextInput style={styles.nomeInput} placeholder="Novo Treino"/>
+                <EvilIcons style={styles.iconInput} name="pencil" size={35} color="black" />
+              </View>
+            </View>
+            <View style={styles.workoutView}>
+
+            </View>
+            <View style={styles.btnsView}>
+              <TouchableOpacity
+                  style={styles.btnCriar}
+                  onPress={props.btnCancelar}
+              >
+                  <Text style={styles.btnText}>Criar Treino</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                  style={styles.btnCancelar}
+                  onPress={props.onClose}
+              >
+                  <Text style={styles.btnText}>Cancelar</Text>
+              </TouchableOpacity>
+            </View>
         </View>
       </Modal>
     )
@@ -28,6 +48,52 @@ const styles = StyleSheet.create({
       },
       backText: {
         color: 'blue'
+      },
+      nameView: {
+        width: '100%',
+      },
+      infosNameView: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent:'center',
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        width: '60%',
+        marginLeft: '5%',
+      },
+      nomeInput: {
+        fontSize: 30,
+        width:'80%',
+      },
+      workoutView: {
+        height: '70%',
+        backgroundColor:'blue',
+      },
+      btnsView:{
+        display:'flex',
+        flexDirection:'row',
+        width: '70%',
+        justifyContent:'space-around'
+      },
+      btnCriar: {
+        backgroundColor: '#E7E7E7',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 50,
+        width: 130
+      },
+      btnCancelar: {
+        height: 50,
+        width: 130,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: '#e7e7e7',
+        borderWidth: 2
+      },
+      btnText: {
+        fontSize: 20
       }
 })
 
